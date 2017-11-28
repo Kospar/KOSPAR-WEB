cd858621ef48a8bce81d037db29e7ccbf1d0321f
chogyuwon <cgw0280@gmail.com>
2017-11-28 19:05:17 +0000

    test commit_v5.2

--- app/admin/reply.rb
+++ app/admin/reply.rb
@@ -1,26 +1,26 @@
-ActiveAdmin.register Reply do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
- menu parent: 'QnA', label: '답변 관리' , priority: 2
+# ActiveAdmin.register Reply do
+# # See permitted parameters documentation:
+# # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+# #
+# # permit_params :list, :of, :attributes, :on, :model
+# #
+# # or
+# #
+#  menu parent: 'QnA', label: '답변 관리' , priority: 2
  
- actions :index, :update, :edit, :destroy
-  index do
-    selectable_column
-    actions
-    id_column
-    Reply.column_names.each do |c|
-      column c.to_sym
-    end
-  end
- permit_params do
-    permitted  [:permitted, :id, :title, :content, :image, :user_id, :qna_id, :created_at, :updated_at]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
- end
- filter :created_at
-end
+#  actions :index, :update, :edit, :destroy
+#   index do
+#     selectable_column
+#     actions
+#     id_column
+#     Reply.column_names.each do |c|
+#       column c.to_sym
+#     end
+#   end
+#  permit_params do
+#     permitted  [:permitted, :id, :title, :content, :image, :user_id, :qna_id, :created_at, :updated_at]
+# #   permitted << :other if params[:action] == 'create' && current_user.admin?
+# #   permitted
+#  end
+#  filter :created_at
+# end
